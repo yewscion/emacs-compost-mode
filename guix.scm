977179d1324843d8a6db9897283fc7bfbbcc7ffb
Christopher Rodriguez <yewscion@gmail.com>
2023-02-13 19:28:32 -0500

    guix.scm: Modified to build correctly, with docs.

--- guix.scm
+++ guix.scm
@@ -8,15 +8,32 @@
  (gnu packages)
  (gnu packages autotools)
  (gnu packages pkg-config)
+ (gnu packages emacs-xyz)
  (gnu packages texinfo)
  (guix gexp))
 
 (package
   (name "emacs-compost-mode")
   (version "0.1.0")
-  (source (local-file (string-append "./"))
+  (source (local-file (getcwd) #:recursive? #t))
   (build-system emacs-build-system)
-  (native-inputs (list texinfo))                       
+  (native-inputs (list texinfo))
+  (propagated-inputs (list emacs-deadgrep
+                           emacs-pdf-tools))
+  (arguments
+   (list
+    #:phases
+    #~(modify-phases %standard-phases
+        (add-before 'install 'make-info
+          (lambda* (#:key inputs #:allow-other-keys)
+            (let ((makeinfo
+                   (search-input-file inputs "/bin/makeinfo")))
+              (system
+               (string-append
+                makeinfo " " "doc/emacs-compost-mode.texi")))))
+        (add-before 'install 'set-home
+          (lambda _
+            (setenv "HOME" "/tmp"))))))
   (synopsis "Notetaking Mode for GNU Emacs")
   (description
    (string-append
